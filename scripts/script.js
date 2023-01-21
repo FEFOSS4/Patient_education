31499335e30713cd5c994892442da7cf513d2d11
AyobSea <Ayob_Sea@hotmail.com>
2023-01-22 01:53:19 +0300

    done

--- scripts/script.js
+++ scripts/script.js
@@ -56,3 +56,26 @@ function generateQrCode() {
     correctLevel: QRCode.CorrectLevel.H,
   });
 }
+
+// ayuob stuff //
+
+var $carousel = $('.carousel').flickity({
+  imagesLoaded: true,
+  percentPosition: false,
+});
+
+var $imgs = $carousel.find('.carousel-cell img');
+// get transform property
+var docStyle = document.documentElement.style;
+var transformProp = typeof docStyle.transform == 'string' ?
+  'transform' : 'WebkitTransform';
+// get Flickity instance
+var flkty = $carousel.data('flickity');
+
+$carousel.on( 'scroll.flickity', function() {
+  flkty.slides.forEach( function( slide, i ) {
+    var img = $imgs[i];
+    var x = ( slide.target + flkty.x ) * -1/3;
+    img.style[ transformProp ] = 'translateX(' + x  + 'px)';
+  });
+});
